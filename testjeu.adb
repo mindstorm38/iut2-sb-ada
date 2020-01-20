7761bd49adddd10a12d6d829084b0252f9d9315b
ROUSSEAU Nathan <roussnat@hostname.iut2.upmf-grenoble.fr>
2020-01-20 09:34:10 +0100

    update

--- testjeu.adb
+++ testjeu.adb
@@ -1,9 +1,17 @@
-with p_virus; use p_virus;
+--with p_virus; use p_virus;
+with text_io; use text_io;
+
 
 
 procedure testjeu is
   --{} => {}
-
+	package p_int_io is new integer_io(integer);
+	use p_int_io;
+	numconf : integer range 1..20;
+	grille : TV_Grille;
+	Pieces : TV_Pieces;
 begin -- testjeu
-
+	InitPartie(grille,pieces);
+	put("Quel est le numéro de la configuration que vous voulez charger ? : ");
+	get(numconf);
 end testjeu;
